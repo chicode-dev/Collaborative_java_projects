3d1c85d99e6055d6a9c4f9b7975e61aabe8551c7
Chimdindu-dev <cchimdindu@gmail.com>
2022-12-22 14:55:00 +0200

    update

---
+++ Calculator/src/calculator/Main.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Chimdi and Samson");
+	}
+
+}
